d64436db551f51f910a8645b30821f2a455b0d93
Shailendra Ahir <shailendraahir@hotmail.com>
2026-05-19 01:12:53 +0530

    Test the installation

--- src/main/java/com/shailahir/actions/Main.java
+++ src/main/java/com/shailahir/actions/Main.java
@@ -1,8 +1,12 @@
 package com.shailahir.actions;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    static void main(String[] args) {
+    static void main(String[] args) throws IOException, InterruptedException {
         String testInput = System.getenv("INPUT_TESTINPUT");
 
         if (testInput == null) {
@@ -10,5 +14,33 @@ public class Main {
         }
 
         System.out.println("Input : " + testInput);
+
+        ProcessBuilder pb = new ProcessBuilder(
+                "kubectl",
+                "version",
+                "--client"
+        );
+
+        pb.redirectErrorStream(true);
+
+        Process process = pb.start();
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream())
+        );
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        int exitCode = process.waitFor();
+
+        System.out.println("Exit code: " + exitCode);
+
+        if (exitCode != 0) {
+            System.exit(exitCode);
+        }
     }
 }
