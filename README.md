06c0e3571df283ecc450b3f3c1ce25c39b34f3ea
Shailendra Ahir <shailendraahir@hotmail.com>
2026-05-19 01:40:54 +0530

    Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-Kubernetes Action
+# Kubernetes Action
 
-Testing kubernetes deployment using custom action
+Deploy to a Kubernetes Cluster using Github action
+
+Usage example
+
+```githubexpressionlanguage
+
+      - name: Deploy to cluster
+        uses: shailahir/kubernetes-action@v0.1.4
+        with:
+          kubeConfig: ${{ secrets.KUBE_CONFIG }}
+
+```
+
+## Currently supports:
+
+Only prints the pods in the console
+
+## Future plan:
+
+- Kubectl full support (Opinionated)
+- 
--- src/main/java/com/shailahir/actions/Main.java
+++ src/main/java/com/shailahir/actions/Main.java
@@ -1,54 +1,19 @@
 package com.shailahir.actions;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
 
     static void main(String[] args) throws IOException, InterruptedException {
-        String testInput = System.getenv("INPUT_TESTINPUT");
-
-        if (testInput == null) {
-            testInput = "Hard coded Test Input";
-        }
-
-        System.out.println("Input : " + testInput);
-
-        ProcessBuilder pb = new ProcessBuilder(
-                "kubectl",
-                "version",
-                "--client"
-        );
-
-        pb.redirectErrorStream(true);
-
-        Process process = pb.start();
-
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(process.getInputStream())
-        );
-
-        String line;
-
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
-
-        int exitCode = process.waitFor();
-
-        System.out.println("Exit code: " + exitCode);
-
-
         String kubeconfig = System.getenv("INPUT_KUBECONFIG");
 
         Path kubeconfigPath = Files.createTempFile("kubeconfig", ".yaml");
 
         Files.writeString(kubeconfigPath, kubeconfig);
 
-        pb = new ProcessBuilder(
+        ProcessBuilder pb = new ProcessBuilder(
                 "kubectl",
                 "get",
                 "pods",
@@ -62,9 +27,9 @@ public class Main {
 
         pb.inheritIO();
 
-        process = pb.start();
+        Process process = pb.start();
 
-        exitCode = process.waitFor();
+        int exitCode = process.waitFor();
 
         Files.deleteIfExists(kubeconfigPath);
 
