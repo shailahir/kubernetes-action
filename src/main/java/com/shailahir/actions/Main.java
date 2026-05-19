b938b4a5f78d06ab1c8152b4e25352d946b9973b
Shailendra Ahir <shailendraahir@hotmail.com>
2026-05-19 10:37:53 +0530

    Fixing path

--- src/main/java/com/shailahir/actions/Main.java
+++ src/main/java/com/shailahir/actions/Main.java
@@ -33,7 +33,7 @@ public class Main {
                 System.out.println("Path = " + path);
 
                 if (path.toFile().exists()) {
-                    this.kubectlExecutor.executeCommand("apply", "-f", path.toAbsolutePath().toString());
+                    this.kubectlExecutor.executeCommand("apply", "-f", path.toString());
                 }
             }
 
